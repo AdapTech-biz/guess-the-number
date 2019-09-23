cf669bf3f2e6816bf5585f571c59a2125d3d7ca6
xyd93 <xyd93@outlook.com>
2019-09-23 11:46:09 -0400

    Game logic implemented with web application. Basic templates set up with thymeleaf.

--- web/src/main/java/adaptech/biz/controller/GameController.java
+++ web/src/main/java/adaptech/biz/controller/GameController.java
@@ -1,14 +1,52 @@
 package adaptech.biz.controller;
 
+import adaptech.biz.service.GameService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 public class GameController {
 
-    @GetMapping("play")
-    public String playGame() {
+    private final GameService gameService;
 
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("play")
+    public String playGame(Model model) {
+        model.addAttribute("mainMessage", gameService.getMainMessage());
+        model.addAttribute("resultMessage", gameService.getResultMessage());
         return "play";
+    }
+
+    @GetMapping("game-over")
+    public String gameOver(Model model) {
+        model.addAttribute("mainMessage", gameService.getMainMessage());
+        model.addAttribute("resultMessage", gameService.getResultMessage());
+        gameService.resetGame();
+        return "game-over";
+    }
+
+    @PostMapping("play")
+    public String guessNumber(@RequestParam int guess) {
+        log.info("Guessed number {}", guess);
+        gameService.setGuess(guess);
+        gameService.checkGameStatus();
+
+        return gameService.isGameOver() ? "redirect:/game-over" : "redirect:/play";
+
     }
 }
